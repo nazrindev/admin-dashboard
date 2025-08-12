8c861bb297b4794de4f20a068b9741c11c4fd4ea
nazrindev <nazreenfathima08gmail.com>
2025-08-12 16:40:58 +0530

    changes in product api and search functionality

--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
-const { createProduct } = require('../controller/product/create');
-const { getProduct, getProductById } = require('../controller/product/fetch');
-const { updateProduct } = require('../controller/product/update');
-const { deleteProduct } = require('../controller/product/delete');
-const upload = require('../middleware/upload');
+const authMiddleware = require("../middleware/auth");
+const { createProduct } = require("../controller/product/create");
+const {
+  getProduct,
+  getProductById,
+  getMerchantProducts,
+  searchProduct,
+} = require("../controller/product/fetch");
+const { updateProduct } = require("../controller/product/update");
+const { deleteProduct } = require("../controller/product/delete");
 
-router.post('/create', authMiddleware, upload.array('imageUrls', 5), createProduct);
-router.get('/get', getProduct);
-router.get('/get/:id', getProductById);
+const upload = require("../middleware/upload");
 
-router.put('/update/:id', authMiddleware, upload.array('imageUrls', 5), updateProduct);
-router.delete('/delete/:id', authMiddleware, deleteProduct);
+router.post(
+  "/create",
+  authMiddleware,
+  upload.array("imageUrls", 5),
+  createProduct
+);
+router.get("/get", getProduct);
+router.get("/get/:id", getProductById);
 
-module.exports = router; 
+router.put(
+  "/update/:id",
+  authMiddleware,
+  upload.array("imageUrls", 5),
+  updateProduct
+);
+router.delete("/delete/:id", authMiddleware, deleteProduct);
+router.get("/get/merchant/:merchantId", getMerchantProducts);
+router.get("/search", searchProduct);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const merchantUser = require('./routes/merchant');
-const connectDB = require('./database/connectDB');
-const storeRoutes = require('./routes/store');
-const customerRoutes = require('./routes/customer');
-const categoryRoutes = require('./routes/category');
-const subcategoryRoutes = require('./routes/subcategory');
-const productRoutes = require('./routes/product');
-const orderRoutes = require('./routes/order');
-const CartRoutes = require('./routes/cart');
-
-app.use(cors({
-    origin: ['http://localhost:4200', 'http://localhost:3000'],
-    credentials: true 
-  }));
+const merchantUser = require("./routes/merchant");
+const connectDB = require("./database/connectDB");
+const storeRoutes = require("./routes/store");
+const customerRoutes = require("./routes/customer");
+const categoryRoutes = require("./routes/category");
+const subcategoryRoutes = require("./routes/subcategory");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const CartRoutes = require("./routes/cart");
 
 app.use(express.json());
-require("dotenv").config();
-app.use('/merchant', merchantUser);
-app.use('/store', storeRoutes); 
-app.use('/customer', customerRoutes);
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 
-app.use('/category', categoryRoutes);
-app.use('/subcategory', subcategoryRoutes);
-app.use('/product', productRoutes);
-app.use('/order', orderRoutes);
-app.use('/cart', CartRoutes)
+require("dotenv").config();
+app.use("/merchant", merchantUser);
+app.use("/store", storeRoutes);
+app.use("/customer", customerRoutes);
+
+app.use("/category", categoryRoutes);
+app.use("/subcategory", subcategoryRoutes);
+app.use("/product", productRoutes);
+app.use("/order", orderRoutes);
+app.use("/cart", CartRoutes);
 connectDB();
 
-app.listen( process.env.PORT , () => {
-    console.log('working');
-})
+app.listen(process.env.PORT, () => {
+  console.log("working");
+});
