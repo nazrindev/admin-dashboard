fdf9b3903b94a35e1085d2eff836105b9fa5b141
nazrindev <nazreenfathima08gmail.com>
2025-11-24 21:34:23 +0530

    remove unwanted files

--- model/subcategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const subcategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-  isActive: { type: Boolean, default: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Subcategory', subcategorySchema);
